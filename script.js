47b819ed5f385129a77fd87c670c0d5b40e4bbe5
Vishanth <81638898+Vishanth21@users.noreply.github.com>
2025-07-22 14:50:22 +0530

    Add color picker and Toggle pen function
    - need to work on background color change

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+ACTIVECOLOR = '#CCFF00';
+
+let penColor = '#ffa200ff',backgroundColor = '#FFFFFF', penActive = false;
+
 const board = document.querySelector('.board');
 for(let i = 0;i < 256; i++) {
     const grid = document.createElement('div');
     grid.className = 'grid';
+    grid.id = 'g' + i;
+    grid.style.backgroundColor = backgroundColor;
     board.appendChild(grid);
 }
+
+const pen = document.querySelector('.pen input');
+const background = document.querySelector('.background input');
+
+pen.addEventListener("input", () => penColor = pen.value);
+background.addEventListener("input", () => backgroundColor = background.value);
+
+const togglePen = document.querySelector('.switch');
+togglePen.addEventListener('click',function () {
+    penActive = !penActive;
+    penActive ? togglePen.style.backgroundColor = ACTIVECOLOR : togglePen.style.backgroundColor = null;
+})
+
+board.addEventListener("mouseover", function (event) {
+    if(penActive) {
+    event.target.style.backgroundColor = penColor;
+    }
+    
+})
